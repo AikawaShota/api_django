db4e05b8b89b9efc3099601ead4b4d7a7aac0bb0
AikawaShota <aikawa.shota24@gmail.com>
2023-06-19 11:04:27 +0900

    add chatgpt api.

--- AItalk/talk/views.py
+++ AItalk/talk/views.py
@@ -1,15 +1,26 @@
 from django.shortcuts import render
 from . import forms
+import openai
+
+
+with open('./chatgpt_key.txt') as f:
+    chatgpt_api_key = f.readline()
+openai.api_key=chatgpt_api_key
 
 # Create your views here.
 def index(request):
     form = forms.ChatForm()
+    user_message = "{何も入力されていません}"
+    chatgpt_message = "{何も入力されていません}"
     if request.method == "POST":
         user_message = "Your message:" + request.POST["message"]
-        chatgpt_message = "ChatGPT message:" + "ChatGPTの返信を代入する"
-    else:
-        user_message = "{何も入力されていません}"
-        chatgpt_message = "{何も入力されていません}"
+        response = openai.ChatCompletion.create(
+            model="gpt-3.5-turbo",
+            messages=[
+                {"role": "user", "content": request.POST["message"]}
+            ]
+        )
+        chatgpt_message = "ChatGPT message:" + response["choices"][0]["message"]["content"].lstrip()
     context = {
         "form": form,
         "user_message": user_message,
