115cbbe3566e944c52011e92f5608ae2fcf350c2
AikawaShota <aikawa.shota24@gmail.com>
2023-06-19 12:03:01 +0900

    Fix typo.

--- AItalk/talk/models.py
+++ AItalk/talk/models.py
@@ -5,5 +5,5 @@ class Dialogue(models.Model):
     role = models.CharField(max_length=64)
     context = models.CharField(max_length=1024)
 
-    def __star__(self):
+    def __str__(self):
         return self.context
