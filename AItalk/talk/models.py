a9d084606354eae033a26fd9f4c4f43c10d8d748
AikawaShota <aikawa.shota24@gmail.com>
2023-06-19 11:57:02 +0900

    Add Dialogue class for models.py.

--- AItalk/talk/models.py
+++ AItalk/talk/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+class Dialogue(models.Model):
+    role = models.CharField(max_length=64)
+    context = models.CharField(max_length=1024)
+
+    def __star__(self):
+        return self.context
